81b9121ce93e38855e37ee4a8f94bb01cf9a4ed0
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-02-07 21:40:27 -0500

    updated

---
+++ java/OOP/OrderItems/Order.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Order {
+    String name = "";
+    double total = 0;
+    boolean ready = false;
+    ArrayList<Object> items = new ArrayList<Object>();
+}
