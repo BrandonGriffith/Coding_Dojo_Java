f4c76cce3545fd4142acc86b6221be6621522616
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-02-08 00:39:49 -0500

    updated

---
+++ java/OOP/BankAccount/TestBank.java
@@ -0,0 +1,6 @@
+public class TestBank {
+    public static void main(String[] args) {
+        BankAccount banka1 = new BankAccount(100, 50);
+        System.out.println(banka1.totalBal());
+    }
+}
