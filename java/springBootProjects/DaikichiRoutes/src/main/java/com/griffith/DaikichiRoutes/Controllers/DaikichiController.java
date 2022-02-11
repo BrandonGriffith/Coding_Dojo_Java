97d03dd44e939c5ca5b8494956b893268e2bbcd1
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-02-10 23:30:13 -0500

    updated

--- java/springBootProjects/DaikichiRoutes/src/main/java/com/griffith/DaikichiRoutes/Controllers/DaikichiController.java
+++ java/springBootProjects/DaikichiRoutes/src/main/java/com/griffith/DaikichiRoutes/Controllers/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.griffith.DaikichiRoutes.Controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +19,15 @@ public class DaikichiController {
 	public String tomorrow() {
 		return "Tomorrow an opportunity will arise, so be sure to be open to new ideas!";
 	}
+	@RequestMapping("/travel/{city}")
+    public String travel(@PathVariable("city") String city){
+    	return "Congratulations! You will soon travel to " + city + "!";
+    }
+	@RequestMapping("/lotto/{numInput}")
+    public String lotto(@PathVariable("numInput") int numInput){
+		if(numInput % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+    }
 }
