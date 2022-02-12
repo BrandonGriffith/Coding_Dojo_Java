f9f81f5d8f0bea7780273715fec6d728a8541ae4
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-02-12 08:48:52 -0500

    updated

--- java/springBootProjects/HelloHuman/src/main/java/com/griffith/HelloHuman/controllers/HumanController.java
+++ java/springBootProjects/HelloHuman/src/main/java/com/griffith/HelloHuman/controllers/HumanController.java
@@ -7,19 +7,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HumanController {
 	@RequestMapping("/")
-    public String index(@RequestParam(value="name", required=false) String firstName, 
-    		@RequestParam(value="last_name", required=false) String lastName,
-    		@RequestParam(value="times", required=false, defaultValue = "0") int times
-    		) {
-        if(firstName != null) {
-        	if(lastName != null) {
-        		if(times > 0) {
-        			return ("Hello " + firstName + " " + lastName + " ").repeat(times);
-        		}
-        		return "Hello " + firstName + " " + lastName;
-        	}
-        	return "Hello " + firstName;
-        }
+	public String index(@RequestParam(value = "name", required = false) String firstName,
+			@RequestParam(value = "last_name", required = false) String lastName,
+			@RequestParam(value = "times", required = false, defaultValue = "0") int times) {
+		if (firstName != null) {
+			if (lastName != null) {
+				if (times > 0) {
+					String result =("Hello " + firstName + " " + lastName + " ");
+					for(int i=1;i<times;i++) {
+						result += result;
+					}
+					return result;
+				}
+				return "Hello " + firstName + " " + lastName;
+			}
+			return "Hello " + firstName;
+		}
 		return "Hello Human";
-    }
+	}
 }
